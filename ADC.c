d15b4f0cca8644fce9803e0977bd284382ce30e7
Ajc605 <36259418+Ajc605@users.noreply.github.com>
2018-02-09 13:48:20 +0000

    First try at initialising ADC2

--- ADC.c
+++ ADC.c
@@ -42,3 +42,44 @@ unsigned int readADC1 (void) {
 	/* Return data value */
 	return (ADC1->DR);
 }
+
+/* Function to intiialise ADC2    */
+
+void ADC2_init(void) {
+	/* Enable clocks */
+	RCC->APB2ENR  |= RCC_APB2ENR_ADC1EN;
+	RCC->AHB1ENR  |= RCC_AHB1ENR_GPIOCEN;
+	
+	/* ADC12_IN15 is the channel we shall use. It is connected to 
+	 * PC5 which is connected to the board edge connectors */
+	GPIOC->MODER = 0x4 << (2 * 4);
+	GPIOC->PUPDR = 0;
+	
+	/* Set ADC to discontinuous conversion mode. */
+	ADC2->CR1 = 0x00;
+	ADC2->CR1 |= ADC_CR1_DISCEN;
+	
+	/* Ensure CR2 is set to zero. This means data will be right aligned, 
+	 * DMA is disabled and there are no extrnal triggers/injected channels */
+	ADC2->CR2 = 0x00;
+	
+	/* Set to one conversion at a time, and set that first conversion 
+	 * to come from channel 15 (connected to PC5) */
+	ADC2->SQR1 &= ~ADC_SQR1_L;
+	ADC2->SQR3 = 15 & ADC_SQR3_SQ1;
+	
+	/* Enable the ADC */
+	ADC2->CR2 |= ADC_CR2_ADON;
+}
+	
+/* function to read ADC and retun value */
+unsigned int read_ADC2 (void) {
+	/* set SWSTART to 1 to start conversion */
+	ADC2->CR2 |= ADC_CR2_SWSTART;
+	
+	/* Wait until End Of Conversion bit goes high */
+	while (!(ADC2->SR & ADC_SR_EOC));
+	
+	/* Return data value */
+	return (ADC2->DR);
+}
