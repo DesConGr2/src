c347baa52f66d8a0cbf51dd0bad30f6682c8261c
JJ <jjds502@york.ac.uk>
2018-04-24 14:35:17 +0100

    Merge branch 'master' into Comms

--- serial_comms.h
+++ serial_comms.h
@@ -2,10 +2,10 @@
 #define SERIAL_COMMS_H
 
 void CommsInit(void);
-void WriteToOutputString(double output);
-void PB_FTDI_Wait_Until_Ready(void);
+void PB_FTDI_Wait_Until_Ready();
 void PB_FTDI_Send(char *OutString, int howMany);
+void WriteToOutputString(double output);
 void WriteDataLogMessage();
-void PB_FTDI_SendNewLine(void);
+void PB_FTDI_SendNewLine();
 
 #endif
